844e0f3001c527500af6dd52737b9fa38160f103
Dylan <57853417+4812571@users.noreply.github.com>
2023-05-02 20:30:43 -0400

    remove doc comment

--- fuzz/fuzz_targets/parse_module.rs
+++ fuzz/fuzz_targets/parse_module.rs
@@ -8,33 +8,6 @@ use deno_ast::MediaType;
 use deno_ast::ParseParams;
 use deno_ast::SourceTextInfo;
 
-/*
-use deno_ast::parse_module;
-use deno_ast::MediaType;
-use deno_ast::ParseParams;
-use deno_ast::SourceTextInfo;
-
-let source_text = "class MyClass {}";
-let text_info = SourceTextInfo::new(source_text.into());
-let parsed_source = parse_module(ParseParams {
-  specifier: "file:///my_file.ts".to_string(),
-  media_type: MediaType::TypeScript,
-  text_info,
-  capture_tokens: true,
-  maybe_syntax: None,
-  scope_analysis: false,
-}).expect("should parse");
-
-// returns the comments
-parsed_source.comments();
-// returns the tokens if captured
-parsed_source.tokens();
-// returns the module (AST)
-parsed_source.module();
-// returns the `SourceTextInfo`
-parsed_source.text_info();
- */
-
 fuzz_target!(|data: &[u8]| {
     let source_text = String::from_utf8_lossy(data);
     let text_info = SourceTextInfo::new(source_text.into());
